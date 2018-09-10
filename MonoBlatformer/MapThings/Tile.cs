a4dd4b00e988430c0eb7736c04b55266db2106f4
illfalcon <kostasminch@gmail.com>
2018-09-10 16:03:08 +0300

    Figuring out tilemaps

---
+++ MonoBlatformer/MapThings/Tile.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoBlatformer.MapThings
+{
+    public enum TileType { Solid, Empty, OneWay }
+
+    public class Tile
+    {
+        private Rectangle _sourceRect;
+        private int _width;
+        private int _height;
+
+        public TileType TileType { get; set; }
+    }
+}
